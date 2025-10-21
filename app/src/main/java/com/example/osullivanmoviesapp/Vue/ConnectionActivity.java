1185cec8db40ed35e57f13f55f7ebd056c455026
Spirituel-Franchesco <jordanseugue@gmail.com>
2025-10-21 19:42:50 -0400

    connection and registration activities done

--- app/src/main/java/com/example/osullivanmoviesapp/Vue/ConnectionActivity.java
+++ app/src/main/java/com/example/osullivanmoviesapp/Vue/ConnectionActivity.java
@@ -3,14 +3,19 @@ package com.example.osullivanmoviesapp.Vue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.osullivanmoviesapp.R;
 
 public class ConnectionActivity extends AppCompatActivity {
 
+    private EditText edNameLogin;
+    private EditText edPasswordLogin;
     private Button RegisterNowbtn;
     private Button Loginbtn;
 
@@ -19,8 +24,15 @@ public class ConnectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
 
+        edNameLogin = findViewById(R.id.edNameLogin);
+        edPasswordLogin = findViewById(R.id.edPasswordLogin);
         RegisterNowbtn = findViewById(R.id.RegisterBtnLogin);
         Loginbtn = findViewById(R.id.LoginBtn);
+
+        // Pré-remplir le champ nom avec le dernier utilisateur
+        SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        String lastName = prefs.getString("name", "");
+        edNameLogin.setText(lastName);
 
         RegisterNowbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,8 +45,28 @@ public class ConnectionActivity extends AppCompatActivity {
         Loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(ConnectionActivity.this, MenuActivity.class);
-                startActivity(i);
+
+                String name = edNameLogin.getText().toString();
+                String password = edPasswordLogin.getText().toString();
+
+                String savedName = prefs.getString("name", "");
+                String savedPassword = prefs.getString("password", "");
+
+                if (name.equals(savedName) && password.equals(savedPassword)) {
+                    Toast.makeText(ConnectionActivity.this, "Connexion réussie !", Toast.LENGTH_LONG).show();
+
+                    Intent i = new Intent(ConnectionActivity.this, MenuActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else if (!savedName.equals(name) && savedPassword.equals(password))
+                {
+                    Toast.makeText(ConnectionActivity.this, "User introuvable", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(ConnectionActivity.this, "User et/ou password incorrect", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/osullivanmoviesapp/Vue/RegistrationActivity.java
+++ app/src/main/java/com/example/osullivanmoviesapp/Vue/RegistrationActivity.java
@@ -2,15 +2,63 @@ package com.example.osullivanmoviesapp.Vue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.osullivanmoviesapp.R;
 
+import java.security.PrivateKey;
+
 public class RegistrationActivity extends AppCompatActivity {
 
+    private EditText edNameRegister;
+    private EditText edPasswordRegister;
+    private EditText edEmailRegister;
+    private Button btnRegister;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        edNameRegister = findViewById(R.id.edNameRegister);
+        edPasswordRegister = findViewById(R.id.edPasswordRegister);
+        edEmailRegister = findViewById(R.id.edEmailRegister);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edNameRegister.getText().toString();
+                String password = edPasswordRegister.getText().toString();
+                String email = edEmailRegister.getText().toString();
+
+                if (name.isEmpty() || email.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(RegistrationActivity.this, "Crédentials invalides", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+
+                    editor.putString("name", name);
+                    editor.putString("email", email);
+                    editor.putString("password", password);
+                    editor.apply();
+
+                    Toast.makeText(RegistrationActivity.this, "Inscription réussie !", Toast.LENGTH_LONG).show();
+                }
+                Intent i = new Intent(RegistrationActivity.this, ConnectionActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
