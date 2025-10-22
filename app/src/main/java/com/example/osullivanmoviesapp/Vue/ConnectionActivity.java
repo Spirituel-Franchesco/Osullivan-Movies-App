3517f6bbfb9c0fcc8fe3299c814c0f4c9829838b
Spirituel-Franchesco <jordanseugue@gmail.com>
2025-10-22 00:27:23 -0400

    Ad/Publicity done

--- app/src/main/java/com/example/osullivanmoviesapp/Vue/ConnectionActivity.java
+++ app/src/main/java/com/example/osullivanmoviesapp/Vue/ConnectionActivity.java
@@ -1,5 +1,6 @@
 package com.example.osullivanmoviesapp.Vue;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,7 +11,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 import com.example.osullivanmoviesapp.R;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 public class ConnectionActivity extends AppCompatActivity {
 
@@ -19,10 +34,13 @@ public class ConnectionActivity extends AppCompatActivity {
     private Button RegisterNowbtn;
     private Button Loginbtn;
 
+    private InterstitialAd mInterstitialAd; // pub interstitielle
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+
 
         edNameLogin = findViewById(R.id.edNameLogin);
         edPasswordLogin = findViewById(R.id.edPasswordLogin);
@@ -33,6 +51,12 @@ public class ConnectionActivity extends AppCompatActivity {
         SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
         String lastName = prefs.getString("name", "");
         edNameLogin.setText(lastName);
+
+        // Initialisation du SDK
+        MobileAds.initialize(this, initializationStatus -> {});
+
+        // Charger la publicité interstitielle
+        loadInterstitialAd();
 
         RegisterNowbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,11 +77,19 @@ public class ConnectionActivity extends AppCompatActivity {
                 String savedPassword = prefs.getString("password", "");
 
                 if (name.equals(savedName) && password.equals(savedPassword)) {
-                    Toast.makeText(ConnectionActivity.this, "Connexion réussie !", Toast.LENGTH_LONG).show();
+                    Toast.makeText(ConnectionActivity.this, "Connexion réussie !", Toast.LENGTH_SHORT).show();
 
-                    Intent i = new Intent(ConnectionActivity.this, MenuActivity.class);
-                    startActivity(i);
-                    finish();
+//                    Intent i = new Intent(ConnectionActivity.this, MenuActivity.class);
+//                    startActivity(i);
+//                    finish();
+
+                    // 👉 Afficher la pub si elle est prête
+                    if (mInterstitialAd != null) {
+                        mInterstitialAd.show(ConnectionActivity.this);
+                    } else {
+                        // Si la pub n’est pas encore prête, aller directement au menu
+                        goToMenu();
+                    }
                 }
                 else if (!savedName.equals(name) && savedPassword.equals(password))
                 {
@@ -69,5 +101,43 @@ public class ConnectionActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void loadInterstitialAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(InterstitialAd interstitialAd) {
+//                        super.onAdLoaded(interstitialAd);
+                        mInterstitialAd = interstitialAd;
+
+                        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+//                                super.onAdDismissedFullScreenContent();
+                                goToMenu();
+                                loadInterstitialAd(); // recharge la prochaine pub
+                            }
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(AdError adError)
+                            {
+                                mInterstitialAd = null;
+                                goToMenu();
+                            }
+                        });
+                    }
+                        @Override
+                        public void onAdFailedToLoad(LoadAdError loadAdError)
+                        {
+                            mInterstitialAd = null;
+                        }
+                    });
+                }
+
+    private void goToMenu() {
+        Intent i = new Intent(ConnectionActivity.this, MenuActivity.class);
+        startActivity(i);
     }
 }
